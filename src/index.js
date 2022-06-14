1e6bfe3998a54d80f623e3857de558da155317e1
nstickler28 <nathanstickler@gmail.com>
2022-06-14 16:44:55 -0700

    Using JSX to reference JS variables

--- src/index.js
+++ src/index.js
@@ -2,15 +2,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+    return 'Click on me!';
+}
+
 // Create a react component
 const App = () => {
+    // Cannot reference JS object inside of JSX where we would show text
+    //  const buttonText = { text: 'Click Me!' };
+    const buttonText = 'Click Me!';
+    const labelText = 'Enter name:';
+
     return (
         <div>
-            <label class="label" for="name">
-                Enter name:
+            <label className="label" for="name">
+                {labelText}
             </label>
             <input id="name" type="text" />
-            <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                {/* {buttonText.text} */}
+                {buttonText}
+            </button>
         </div >
     );
 };
